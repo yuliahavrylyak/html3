da0db7012def1f32e2e705b4cc12472caab8e88c
yuliahavrylyak <yliahavrylyak@gmail.com>
2021-10-12 19:32:34 +0000

    Changes

--- menu.js
+++ menu.js
@@ -1,7 +1,6 @@
-// search-box open close js code
+
 let navbar = document.querySelector(".navbar");
 let searchBox = document.querySelector(".search-box .bx-search");
-// let searchBoxCancel = document.querySelector(".search-box .bx-x");
 
 searchBox.addEventListener("click", ()=>{
   navbar.classList.toggle("showInput");
@@ -12,7 +11,6 @@ searchBox.addEventListener("click", ()=>{
   }
 });
 
-// sidebar open close js code
 let navLinks = document.querySelector(".nav-links");
 let menuOpenBtn = document.querySelector(".navbar .bx-menu");
 let menuCloseBtn = document.querySelector(".nav-links .bx-x");
@@ -23,8 +21,6 @@ menuCloseBtn.onclick = function() {
 navLinks.style.left = "-100%";
 }
 
-
-// sidebar submenu open close js code
 let htmlcssArrow = document.querySelector(".htmlcss-arrow");
 htmlcssArrow.onclick = function() {
  navLinks.classList.toggle("show1");
